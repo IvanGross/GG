cc06702f5a91aa039316a430b3708d17d9a5042b
IvanGross <ivangross925@gmail.com>
2019-02-25 18:41:52 +0300

    GG2

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +7,30 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
-    class Program
+    class WorkShop
     {
-        static void Main(string[] args)
+        private string name;
+        public string Name { get; set; }
+        private string head;
+        public string Head { get; set; }
+        private int number;
+        public int Number { get; set; }
+        public WorkShop()
         {
+            Name = "Производство молока";
+            Head = "Федоров Иван Васильевич";
+            Number = 50;
         }
-    }
-}
+
+        public void Fill()
+        {
+            Console.WriteLine("Введите данные");
+            Name = Console.ReadLine();
+            Head = Console.ReadLine();
+            Number = Convert.ToInt32(Console.ReadLine());
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"Название цеха: {Name}, Начальник цеха: {Head}, Количество работающих: {Number}");
+        }
